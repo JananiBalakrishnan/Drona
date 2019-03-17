c99d25287b9173d4afa0f04fdf0d4131b09d100e
Janani Balakrishnan <jaanu@Jananis-MacBook-Air.local>
2019-03-16 20:14:32 -0700

    Basic Project Setup

---
+++ src/main/java/DayOne.java
@@ -0,0 +1,25 @@
+
+public class DayOne {
+
+	/*
+	 * Topics Covered : 
+	 * Basic Environment Setup - git, sts4, java, maven 
+	 */
+	
+	public static void main(String args[]) {
+		System.out.println(" ******** Basic Programming ******* ");
+		
+		String name="java";
+		
+		// IF - ELSE LOOP
+		if(name=="java") {
+			System.out.println(" ****** Success! ");
+		}
+		else{
+			System.out.println(" ****** Failure! ");
+		}
+		
+	}	
+	
+	
+}
---
+++ src/main/java/com/lotus/drona/DayTwo.java
@@ -0,0 +1,11 @@
+package com.lotus.drona;
+
+public class DayTwo {
+
+	/*
+	 * Learn Basics - if-else,for loop,while,switch cases 
+	 * Access Modifiers - public,private, protected 
+	 * Static, Singleton - Usage 
+	 * Write a TestNG Test, Read values from DataProvider
+	 */
+}
