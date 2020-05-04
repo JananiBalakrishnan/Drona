51626e34756fad473193b0f2f6e44d9d8100ff4a
JananiBalakrishnan <jananibalakrishnann@gmail.com>
2020-05-04 16:12:25 -0700

    Update DayTwo.java

--- src/main/java/com/lotus/drona/DayTwo.java
+++ src/main/java/com/lotus/drona/DayTwo.java
@@ -8,4 +8,14 @@ public class DayTwo {
 	 * Static, Singleton - Usage 
 	 * Write a TestNG Test, Read values from DataProvider
 	 */
+	
+	//Instead of traditional for loop, use IntStream
+	for (int i = 0; i < 10; i++) {
+	System.out.println(i);
+	}
+
+	//Using Intstream
+	 IntStream.range(0, size).forEach(
+                    size -> System.out.println("******* size")
+            );
 }
